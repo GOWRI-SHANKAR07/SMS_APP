635513a5aa09c8f43b1cf652bac64d46496d512a
GOWRI-SHANKAR07 <gowrishankarneelakandan@gmail.com>
2023-09-13 18:29:13 +0530

    Data: Courses Data: Updated

---
+++ src/data/Courses.js
@@ -0,0 +1,31 @@
+export const coursesData = [
+    {
+        id: 1,
+        code: 'DSFC109',
+        name: 'Fundamentals of Copy Writing',
+    },
+
+    {
+        id: 2,
+        code: 'TSPC111',
+        name: 'University Choir 1',
+    },
+
+    {
+        id: 3,
+        code: 'TSSE142',
+        name: 'Live Sound Technology',
+    },
+
+    {
+        id: 4,
+        code: 'TSSE211',
+        name: 'Advanced Recording Techniques',
+    },
+
+    {
+        id: 5,
+        code: 'TSSE212',
+        name: 'Microphone Technology',
+    },
+]
