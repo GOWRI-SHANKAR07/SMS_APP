79038d2600531122965f8c278d1420795325901c
GOWRI-SHANKAR07 <gowrishankarneelakandan@gmail.com>
2023-09-14 17:58:29 +0530

    Components: RegCoursesCard: Registered Courses list Card: Completed

---
+++ src/components/RegCouresesCard.jsx
@@ -0,0 +1,78 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { Entypo, AntDesign } from '@expo/vector-icons';
+import { Colors, heightPixel, pixelSizeHorizontal, pixelSizeVertical, widthPixel } from '../Constants/Theme'
+import { styles } from '../styles/RegCoursesCard'
+
+
+const RegCoursesCard = ({ items }) => {
+
+    const [open, setOpen] = useState(false)
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity
+                style={styles.innerCont}
+                onPress={() => open ? setOpen(false) : setOpen(true)}
+            >
+                <View>
+                    <Text style={styles.txt}>{items.code}</Text>
+                    <Text style={styles.txt}>{items.name}</Text>
+                </View>
+                {open ?
+                    <Entypo
+                        name='chevron-small-up'
+                        size={30}
+                        color={Colors.black}
+                    />
+                    :
+                    <Entypo
+                        name='chevron-small-down'
+                        size={30}
+                        color={Colors.black}
+                    />
+                }
+            </TouchableOpacity>
+            {open ?
+                <View style={styles.detailsCont}>
+                    <View style={styles.subCont}>
+                        <View style={styles.innerSub}>
+                            <Text style={styles.subTxt}>{items.lecture}</Text>
+                            {items.lectureCompleted ?
+                                ''
+                                :
+                                <AntDesign
+                                    name="exclamationcircle"
+                                    size={15}
+                                    color={Colors.red}
+                                    style={styles.warnIcon}
+                                />
+                            }
+                        </View>
+                        <View style={styles.innerSub}>
+                            <Text style={styles.subTxt}>{items.lab}</Text>
+                            {items.labCompleted ?
+                                ''
+                                :
+                                <AntDesign
+                                    name="exclamationcircle"
+                                    size={15}
+                                    color={Colors.red}
+                                    style={styles.warnIcon}
+                                />
+                            }
+                        </View>
+                    </View>
+                </View>
+                :
+                ''
+            }
+            <View style={styles.statusCont}>
+                <Text style={styles.statusTxt}>{items.registered ? 'Registered' : 'Pending'}</Text>
+                <Text style={[styles.statusTxt, { color: Colors.greyDark }]}>De-Register</Text>
+            </View>
+        </View>
+    )
+}
+
+export default RegCoursesCard
