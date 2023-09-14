8293240cf9469d2da420d1fb05d418e7d33cc05b
GOWRI-SHANKAR07 <gowrishankarneelakandan@gmail.com>
2023-09-14 18:02:13 +0530

    Screens: RegisteredCourses: Registered courses listing UI: Completed

---
+++ src/screens/RegisteredCourses.jsx
@@ -0,0 +1,18 @@
+import { View } from 'react-native'
+import React from 'react'
+import RegCoursesCard from '../components/RegCouresesCard'
+import { coursesData } from '../data/Courses'
+import { styles } from '../styles/RegisteredCourses'
+
+const RegisteredCoureses = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.coursesCont}>
+        {coursesData.map(item => <RegCoursesCard key={item.id} items={item} />
+        )}
+      </View>
+    </View>
+  )
+}
+
+export default RegisteredCoureses
