2486f352b60babd95f780441a10d9b1df4ef8948
GOWRI-SHANKAR07 <gowrishankarneelakandan@gmail.com>
2023-09-15 13:17:49 +0530

    Components: Details Contnet view animation: Inprogress

--- src/components/RegCouresesCard.jsx
+++ src/components/RegCouresesCard.jsx
@@ -3,17 +3,52 @@ import React, { useState } from 'react'
 import { Entypo, AntDesign } from '@expo/vector-icons';
 import { Colors, heightPixel, pixelSizeHorizontal, pixelSizeVertical, widthPixel } from '../Constants/Theme'
 import { styles } from '../styles/RegCoursesCard'
+import Animated, { Easing, withTiming, useSharedValue, useAnimatedStyle, FlipInXUp, FlipOutXDown, SlideInUp, SlideInDown, ZoomIn, ZoomOut, FadeOut, SlideInLeft, SlideInRight } from 'react-native-reanimated';
+
 
 
 const RegCoursesCard = ({ items }) => {
 
-    const [open, setOpen] = useState(false)
+    const [open, setOpen] = useState(false);
+    // const animationValue = useSharedValue(0); // Initialize it to 0
+
+    // const containerStyle = useAnimatedStyle(() => {
+    //     return {
+    //         height: animationValue.value, // Apply the animated height
+    //     };
+    // });
+
+    // const toggleOpen = () => {
+    //     // Update the animation value based on the open state
+    //     animationValue.value = withTiming(open ? 0 : 80, {
+    //         duration: 300, // Adjust the duration as needed
+    //         easing: Easing.inOut(Easing.ease), // Adjust the easing function as needed
+    //     });
+
+    //     // Toggle the open state
+    //     setOpen(!open);
+    // };
+
+    // const toggleClose = () => {
+    //     // Update the animation value based on the open state
+    //     animationValue.value = withTiming(open ? 20 : 0, {
+    //         duration: 300, // Adjust the duration as needed
+    //         easing: Easing.inOut(Easing.ease) // Adjust the easing function as needed
+    //     });
+
+    //     // Toggle the open state
+    //     setOpen(!open);
+    // };
+
 
     return (
         <View style={styles.container}>
-            <View style={styles.outerCont}>
+            <View style={styles.outerCont}
+            >
                 <TouchableOpacity
-                    style={styles.innerCont}
+                    style={[
+                        styles.innerCont,
+                    ]}
                     onPress={() => open ? setOpen(false) : setOpen(true)}
                 >
                     <View>
@@ -35,7 +70,11 @@ const RegCoursesCard = ({ items }) => {
                     }
                 </TouchableOpacity>
                 {open ?
-                    <View style={styles.detailsCont}>
+                    <Animated.View
+                        entering={SlideInUp}
+                        exiting={SlideInDown.duration(30)}
+                        style={styles.detailsCont}
+                    >
                         <View style={styles.subCont}>
                             <View style={styles.innerSub}>
                                 <Text style={styles.subTxt}>{items.lecture}</Text>
@@ -64,7 +103,7 @@ const RegCoursesCard = ({ items }) => {
                                 }
                             </View>
                         </View>
-                    </View>
+                    </Animated.View>
                     :
                     ''
                 }
