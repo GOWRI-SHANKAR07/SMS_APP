e93f5fc08566de23ffe8c3320c1de09a69307e68
GOWRI-SHANKAR07 <gowrishankarneelakandan@gmail.com>
2023-09-15 13:19:58 +0530

    Screens: RegisteredCourses: Boder Line Added: Updated

--- src/screens/RegisteredCourses.jsx
+++ src/screens/RegisteredCourses.jsx
@@ -1,4 +1,4 @@
-import { View } from 'react-native'
+import { ScrollView, View } from 'react-native'
 import React from 'react'
 import RegCoursesCard from '../components/RegCouresesCard'
 import { coursesData } from '../data/Courses'
@@ -7,10 +7,12 @@ import { styles } from '../styles/RegisteredCourses'
 const RegisteredCoureses = () => {
   return (
     <View style={styles.container}>
+      <ScrollView>
       <View style={styles.coursesCont}>
         {coursesData.map(item => <RegCoursesCard key={item.id} items={item} />
         )}
       </View>
+      </ScrollView>
     </View>
   )
 } 
