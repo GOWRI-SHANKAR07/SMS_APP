080dc9e1b7f852ed591e7a1571853912bbf60885
GOWRI-SHANKAR07 <gowrishankarneelakandan@gmail.com>
2023-09-19 18:31:21 +0530

    Components: AddOptions: Removing the options: Inprogress

--- src/components/AddOption.jsx
+++ src/components/AddOption.jsx
@@ -1,11 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { styles } from '../styles/Poll'
-import { pixelSizeVertical } from '../Constants/Theme'
+import { Colors, pixelSizeVertical } from '../Constants/Theme'
 import TextInputBox from './TextInputBox'
 import Animated, { SlideInLeft } from 'react-native-reanimated'
+import { useAppContext } from '../context/AppContext'
 
-const AddOption = ({ items, value, onChangeText, animate }) => {
+const AddOption = ({ items, value, onChangeText, animate, }) => {
+
+    const { options, setOptions } = useAppContext();
+
+    const handleRemoveOption = (items) => {
+        const index = options.indexOf(items);
+        options.splice(index, 1);
+        console.log(options);
+    }
+
+
+
     return (
         <View
             style={
@@ -17,7 +29,29 @@ const AddOption = ({ items, value, onChangeText, animate }) => {
                 ]
             }
         >
-            <Animated.Text style={styles.titleTxt} entering={animate ? SlideInLeft : ''}>{items}</Animated.Text>
+            <View style={styles.titleCont}>
+                <Animated.Text
+                    style={styles.titleTxt}
+                    entering={animate ? SlideInLeft : ''}
+                >
+                    {items}
+                </Animated.Text>
+                {options.indexOf(items) !== 0 &&  options.indexOf(items) !== 1  ?
+                    <TouchableOpacity
+                        onPress={() => handleRemoveOption(items)}
+                    >
+                        <Animated.Text
+                            style={[styles.titleTxt, { color: Colors.primary }]}
+                            entering={animate ? SlideInLeft : ''}
+                        >
+                            Remove
+                        </Animated.Text>
+                    </TouchableOpacity>
+                    :
+                    ''
+                }
+
+            </View>
             <Animated.View entering={animate ? SlideInLeft.duration(500) : ''}>
                 <TextInputBox
                     maxLength={140}
