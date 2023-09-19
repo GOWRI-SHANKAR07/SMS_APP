38cbb29658c6588f8181eb327b6f5c4102a7b906
GOWRI-SHANKAR07 <gowrishankarneelakandan@gmail.com>
2023-09-19 18:33:02 +0530

    Stack: AppStack: Global context state created: Updated

--- src/stack/AppStack.jsx
+++ src/stack/AppStack.jsx
@@ -1,20 +1,23 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import BottomTab from './BottomTab';
+import ContextProvider from '../context/AppContext';
 
 // App Stack creation
 const Stack = createNativeStackNavigator();
 
 export default function AppStack() {
     return (
-        <NavigationContainer>
-            <Stack.Navigator
-                screenOptions={{
-                    headerShown: false
-                }}
-            >
-                <Stack.Screen name='Homes' component={BottomTab} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <ContextProvider>
+            <NavigationContainer>
+                <Stack.Navigator
+                    screenOptions={{
+                        headerShown: false
+                    }}
+                >
+                    <Stack.Screen name='Homes' component={BottomTab} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </ContextProvider>
     );
 }
