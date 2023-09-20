3f706a9cffa351a6b74d36d9e87e8fe48b230f76
GOWRI-SHANKAR07 <gowrishankarneelakandan@gmail.com>
2023-09-20 18:17:48 +0530

    Screen: Poll Screen: remove function updated for dynamic removal: Updated

--- src/screens/Poll.jsx
+++ src/screens/Poll.jsx
@@ -1,5 +1,5 @@
 import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { styles } from '../styles/Poll'
 import { EvilIcons } from '@expo/vector-icons';
 import TextInputBox from '../components/TextInputBox';
@@ -9,122 +9,84 @@ import AddOption from '../components/AddOption';
 import Animated, { Easing, SlideInLeft, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 import { useAppContext } from '../context/AppContext';
 
-
 const Poll = ({ }) => {
     const [question, setQuestion] = useState('');
     const [inputValues, setInputValues] = useState({});
     const [characCount, setCharacCount] = useState(0);
     const [animate, setAnimate] = useState(false);
-    const [removal, setRemoval] = useState();
-    const [index, setIndex] = useState();
 
     const { options, setOptions } = useAppContext();
-
-    // animation starting point
-    const animatedValueLeft1 = useSharedValue(-50);
-    const animatedValueLeft2 = useSharedValue(-70);
-
-    // checking to start the removal animation
-    isRemoving = options.indexOf(removal) === index;
-
-
-    // handling lecture content sliding from left
-    const slideInFromLeft1 = () => {
-        animatedValueLeft1.value = withTiming(options ? 0 : 60, {
-            duration: 200, // Adjust the duration as needed
-            easing: Easing.inOut(Easing.linear),
-        });
-    };
-
-    // handling lab content sliding from left
-    const slideInFromLeft2 = () => {
-        animatedValueLeft2.value = withTiming(options ? 0 : 60, {
-            duration: 500, // Adjust the duration as needed
-            easing: Easing.inOut(Easing.linear),
-        });
-    };
-
-    // handling lecture content sliding into left
-    const slideInToLeft1 = () => {
-        animatedValueLeft1.value = withTiming(options ? -380 : 0, {
-            duration: 500, // Adjust the duration as needed
-            easing: Easing.inOut(Easing.linear),
-        });
-    };
-
-    // handling lab content sliding into left
-    const slideInToLeft2 = () => {
-        animatedValueLeft2.value = withTiming(options ? -380 : 0, {
-            duration: 200, // Adjust the duration as needed
-            easing: Easing.inOut(Easing.linear),
-        });
-    };
-
-    // lecture container style for animation
-    const containerLeftStyle1 = useAnimatedStyle(() => {
-
-        return {
-            transform: [{ translateX: animatedValueLeft1.value }],
-        };
-    });
-
-    // lab container style for animation
-    const containerLeftStyle2 = useAnimatedStyle(() => {
-        return {
-            transform: [{ translateX: animatedValueLeft2.value }],
-        };
-    });
+    const [totalOptions, setTotalOptions] = useState(2);
+    const [optionTextValues, setOptionTextValues] = useState({});
 
     const handleRemoveOption = (removedItem) => {
-        const updatedOptions = options.filter((item) => item !== removedItem);
-        const removed = options.indexOf(removedItem);
-        setRemoval(removedItem);
-        setIndex(removed)
-        options.map(item => {
-            if (options.indexOf(removal) === options.indexOf(item)) {
-                slideInToLeft1();
-                slideInToLeft2();
-            }
+        // Find the index of the removed option
+        const removedIndex = options.indexOf(removedItem);
+      
+        if (removedIndex !== -1 && removedIndex < totalOptions - 1) {
+          // Create a copy of the current options and optionTextValues
+          const updatedOptions = [...options];
+          const updatedOptionTextValues = { ...optionTextValues };
+      
+          // Replace the content of the removed option with the content of the next option
+          const removedLabel = updatedOptions[removedIndex];
+          const nextLabel = updatedOptions[removedIndex + 1];
+      
+          updatedOptionTextValues[removedLabel] = optionTextValues[nextLabel];
+      
+          // Remove the next option from the options list and optionTextValues
+          updatedOptions.splice(removedIndex + 1, 1);
+          delete updatedOptionTextValues[nextLabel];
+      
+          // Update the state with the new options and values
+          setOptions(updatedOptions);
+          setOptionTextValues(updatedOptionTextValues);
+          setTotalOptions(totalOptions - 1);
         }
-        )
+      };
+      
 
-        setTimeout(() => {
-            setOptions(updatedOptions);
-        }, 800);
-    };
-
-    console.log(index, "Removed index");
-    console.log(options.indexOf(removal), "arr");
+    console.log(optionTextValues, 'ppp');
+    // console.log(inputValues, "ipp");
+    // console.log(options, 'op');
 
     // handle add option
     const handleAddOption = () => {
-        if (options.length >= 5) {
+        if (totalOptions >= 5) {
             alert('You can only add up to 5 items.');
-            return; // Do not add a new item if the limit is reached
+            return;
         }
+
         setAnimate(true);
-        // Create a new item (e.g., 'Option X', where X is the next number)
-        const newOption = `Option ${options.length + 1} *`;
+        const newOptionLabel = `Option ${totalOptions + 1} *`;
 
-        slideInFromLeft1();
-        slideInFromLeft2();
+        // Update options and increment the totalOptions count
+        setOptions([...options, newOptionLabel]);
+        setTotalOptions(totalOptions + 1);
 
-        // Update the items array with the new item
-        setOptions([...options, newOption]);
+        // Add an empty text input value for the new option
+        setOptionTextValues({
+            ...optionTextValues,
+            [newOptionLabel]: '',
+        });
     };
 
     // store input changes to object 
     const handleInputChange = (key, value) => {
         setInputValues({ ...inputValues, [key]: value });
-        setCharacCount(value.length)
+        setCharacCount(value.length);
+
+        // Update the text input value in the state
+        setOptionTextValues({
+            ...optionTextValues,
+            [key]: value,
+        });
     };
 
     // Saving all the user inputs
     const handleSave = () => {
         setInputValues({ ...inputValues, ['Question']: question });
     }
-
-    console.log(options);
 
     return (
         <View style={styles.container}>
@@ -162,31 +124,16 @@ const Poll = ({ }) => {
                                 Charater left : {question.length}/140
                             </Text>
                         </View>
-                        {options.map(item =>
-                            <>
-                                <AddOption
-                                    animate={animate}
-                                    key={item}
-                                    items={item}
-                                    onChangeText={(text) => handleInputChange(item, text)}
-                                    onRemoveOption={handleRemoveOption}
-                                    containerLeftStyle1={containerLeftStyle1}
-                                    removal={removal}
-                                    index={index}
-                                />
-                                <Animated.Text
-                                    style={[
-                                        styles.charactersTxt,
-                                        options.indexOf(item) !== 0 && options.indexOf(item) !== 1 && isRemoving
-                                            ? containerLeftStyle2
-                                            : ''
-                                    ]}
-                                // entering={animate ? SlideInLeft.duration(700) : ''}
-                                >
-                                    Charater left : {characCount}/140
-                                </Animated.Text>
-                            </>
-                        )}
+                        {options.map((item, index) => (
+                            <AddOption
+                                animate={animate}
+                                key={item}
+                                items={item}
+                                value={optionTextValues[item]}
+                                onChangeText={(text) => handleInputChange(item, text)}
+                                onRemoveOption={handleRemoveOption}
+                            />
+                        ))}
                         <View>
                             <TouchableOpacity
                                 style={styles.addCont}
@@ -215,4 +162,4 @@ const Poll = ({ }) => {
     )
 }
 
-export default Poll
+export default Poll;
