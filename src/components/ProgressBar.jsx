243ef2b26f90fa5ba0a2ece8e0d3fd78b433a6df
GOWRI-SHANKAR07 <gowrishankarneelakandan@gmail.com>
2023-09-21 19:17:19 +0530

    Component: ProgressBAr: poll answering progress bar with animation added: Completed

---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,137 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { Colors, heightPixel, pixelSizeVertical } from '../Constants/Theme';
+import Animated, { Easing, useSharedValue, useAnimatedStyle, withSpring, withTiming, SlideInLeft } from 'react-native-reanimated';
+import { styles } from '../styles/ProgressBar';
+import { Ionicons } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
+
+const data = [
+    {
+        option: 'Private',
+        percentage: 24,
+    },
+    {
+        option: 'Public',
+        percentage: 53,
+    },
+    {
+        option: 'Public Permissioned',
+        percentage: 14,
+    },
+    {
+        option: 'Permissioned',
+        percentage: 8,
+    },
+];
+
+const ProgressBar = ({ item, isSelected, selectedOption }) => {
+    const animatedValueLeft = useSharedValue(0);
+
+    const widthAnimation = useSharedValue(1);
+
+    if (isSelected) {
+        widthAnimation.value = withTiming(1, {
+            duration: 500,
+            easing: Easing.linear,
+        });
+    } else {
+        widthAnimation.value = withTiming(0, {
+            duration: 500,
+            easing: Easing.linear,
+        });
+    }
+
+
+    const containerLeftStyle = useAnimatedStyle(() => {
+        return {
+            transform: [{ translateX: animatedValueLeft.value }],
+            width: (widthAnimation.value * item.percentage) + '%',
+        };
+    });
+
+
+
+    return (
+        <View style={styles.progressBar}>
+            {isSelected ?
+                <Animated.View
+                    style={[
+                        styles.progress,
+                        {
+                            height: heightPixel(40),
+                            backgroundColor: isSelected ? Colors.greyMessage : Colors.white,
+                        },
+                        containerLeftStyle
+                    ]}
+                />
+                :
+                ''
+            }
+            <Text style={[
+                styles.optionTxt,
+                {
+                    position: isSelected ? 'absolute' : 'relative',
+                    paddingTop: pixelSizeVertical(10),
+                    paddingLeft: isSelected ? pixelSizeVertical(10) : ''
+                }
+            ]}>
+                {item.option}
+            </Text>
+            {item === selectedOption ?
+                <Ionicons
+                    name="checkmark-circle"
+                    size={24}
+                    color={Colors.green}
+                    style={styles.selectedIcon}
+                />
+                :
+                ''
+            }
+            {isSelected ?
+                <View style={styles.percentageCont}>
+                    <Text style={styles.percentageTxt}>{item.percentage}%</Text>
+                </View>
+                :
+                ''
+            }
+        </View>
+    );
+};
+
+const Polling = () => {
+    const [selectedOption, setSelectedOption] = useState(null);
+    const [animate, setAnimate] = useState(false);
+
+    const handleAnimate = (item) => {
+        setSelectedOption(item);
+        setAnimate(true);
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.pollCont}>
+                <Text style={styles.questionTxt}>Bitcoin is based on _________ blockchain?</Text>
+                {data.map((item, index) => (
+                    <TouchableOpacity
+                        key={index}
+                        onPress={() => handleAnimate(item)}
+                    >
+                        <ProgressBar item={item} isSelected={animate} selectedOption={selectedOption} />
+                    </TouchableOpacity>
+                ))}
+                <View style={styles.voteCont}>
+                    <Text style={styles.voteTxt}>12 votes</Text>
+                    <Entypo name="dot-single" size={24} color="black" />
+                    <Text style={styles.voteTxt}>7d left</Text>
+                    <Entypo name="dot-single" size={24} color="black" />
+                    <Text style={[styles.voteTxt, { color: Colors.primary }]}>Undo</Text>
+                </View>
+            </View>
+        </View>
+    );
+};
+
+
+
+export default Polling;
